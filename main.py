36dab7941718757438e149871d0117a6ab03083e
godmoves <mankitpong@gmail.com>
2018-10-22 11:06:44 +0800

    using lovasz loss for second stage training

--- main.py
+++ main.py
@@ -96,6 +96,94 @@ def my_iou_metric_2(label, pred):
     return tf.py_func(get_iou_vector, [label, pred > 0], tf.float64)
 
 
+# code download from: https://github.com/bermanmaxim/LovaszSoftmax
+def lovasz_grad(gt_sorted):
+    """
+    Computes gradient of the Lovasz extension w.r.t sorted errors
+    See Alg. 1 in paper
+    """
+    gts = tf.reduce_sum(gt_sorted)
+    intersection = gts - tf.cumsum(gt_sorted)
+    union = gts + tf.cumsum(1. - gt_sorted)
+    jaccard = 1. - intersection / union
+    jaccard = tf.concat((jaccard[0:1], jaccard[1:] - jaccard[:-1]), 0)
+    return jaccard
+
+
+# --------------------------- BINARY LOSSES ---------------------------
+
+def lovasz_hinge(logits, labels, per_image=True, ignore=None):
+    """
+    Binary Lovasz hinge loss
+      logits: [B, H, W] Variable, logits at each pixel (between -infinity and +infinity)
+      labels: [B, H, W] Tensor, binary ground truth masks (0 or 1)
+      per_image: compute the loss per image instead of per batch
+      ignore: void class id
+    """
+    if per_image:
+        def treat_image(log_lab):
+            log, lab = log_lab
+            log, lab = tf.expand_dims(log, 0), tf.expand_dims(lab, 0)
+            log, lab = flatten_binary_scores(log, lab, ignore)
+            return lovasz_hinge_flat(log, lab)
+        losses = tf.map_fn(treat_image, (logits, labels), dtype=tf.float32)
+        loss = tf.reduce_mean(losses)
+    else:
+        loss = lovasz_hinge_flat(*flatten_binary_scores(logits, labels, ignore))
+    return loss
+
+
+def lovasz_hinge_flat(logits, labels):
+    """
+    Binary Lovasz hinge loss
+      logits: [P] Variable, logits at each prediction (between -infinity and +infinity)
+      labels: [P] Tensor, binary ground truth labels (0 or 1)
+      ignore: label to ignore
+    """
+
+    def compute_loss():
+        labelsf = tf.cast(labels, logits.dtype)
+        signs = 2. * labelsf - 1.
+        errors = 1. - logits * tf.stop_gradient(signs)
+        errors_sorted, perm = tf.nn.top_k(errors, k=tf.shape(errors)[0], name="descending_sort")
+        gt_sorted = tf.gather(labelsf, perm)
+        grad = lovasz_grad(gt_sorted)
+        # elu doesn't perform better
+        loss = tf.tensordot(tf.nn.relu(errors_sorted), tf.stop_gradient(grad), 1, name="loss_non_void")
+        return loss
+
+    # deal with the void prediction case (only void pixels)
+    loss = tf.cond(tf.equal(tf.shape(logits)[0], 0),
+                   lambda: tf.reduce_sum(logits) * 0.,
+                   compute_loss,
+                   strict=True,
+                   name="loss"
+                   )
+    return loss
+
+
+def flatten_binary_scores(scores, labels, ignore=None):
+    """
+    Flattens predictions in the batch (binary case)
+    Remove labels equal to 'ignore'
+    """
+    scores = tf.reshape(scores, (-1,))
+    labels = tf.reshape(labels, (-1,))
+    if ignore is None:
+        return scores, labels
+    valid = tf.not_equal(labels, ignore)
+    vscores = tf.boolean_mask(scores, valid, name='valid_scores')
+    vlabels = tf.boolean_mask(labels, valid, name='valid_labels')
+    return vscores, vlabels
+
+def lovasz_loss(y_true, y_pred):
+    y_true, y_pred = K.cast(K.squeeze(y_true, -1), 'int32'), K.cast(K.squeeze(y_pred, -1), 'float32')
+    #logits = K.log(y_pred / (1. - y_pred))
+    logits = y_pred #Jiaxin
+    loss = lovasz_hinge(logits, y_true, per_image = True, ignore = None)
+    return loss
+
+
 def RLenc(img, order='F', format=True):
     """
     img is binary mask image, shape (r,c)
@@ -295,10 +383,41 @@ def main():
                             callbacks=[model_checkpoint, reduce_lr, early_stopping], 
                             verbose=2)
 
+    # second stage using lovasz loss
+    model_pre = load_model(model_pre_name, custom_objects={'my_iou_metric': my_iou_metric})
+    # remove layter activation layer and use losvasz loss
+    input_x = model_pre.layers[0].input
+    output_layer = model_pre.layers[-1].input
+
+    # build the final predict model
+    model = Model(input_x, output_layer)
+
+    # lovasz_loss need input range (-∞，+∞), so cancel the last "sigmoid" activation  
+    # Then the default threshod for pixel prediction is 0 instead of 0.5, as in my_iou_metric_2.
+    model.compile(loss=lovasz_loss, optimizer='adam', metrics=[my_iou_metric_2])
+
+    model_name = 'resnet.model'
+
+    early_stopping = EarlyStopping(monitor='val_my_iou_metric_2', mode='max',
+                                   patience=20, verbose=1)
+    model_checkpoint = ModelCheckpoint(model_name, monitor='val_my_iou_metric_2', 
+                                       mode='max', save_best_only=True, verbose=1)
+    reduce_lr = ReduceLROnPlateau(monitor='val_my_iou_metric_2', mode='max',
+                                  factor=0.5, patience=5, min_lr=0.0001, verbose=1)
+
+    epochs = 50
+    batch_size = 32
+    history = model.fit(x_train, y_train,
+                        validation_data=[x_valid, y_valid], 
+                        epochs=epochs,
+                        batch_size=batch_size,
+                        callbacks=[model_checkpoint,reduce_lr,early_stopping], 
+                        verbose=2)
+
 
     # restore the best model
-    model = load_model(model_pre_name, custom_objects={'my_iou_metric_2': my_iou_metric_2,
-                                                       'lovasz_loss': lovasz_loss})
+    model = load_model(model_name, custom_objects={'my_iou_metric_2': my_iou_metric_2,
+                                                   'lovasz_loss': lovasz_loss})
 
     preds_valid = predict_result(model, x_valid, img_size_target)
 
